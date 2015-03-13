d419270e4e8969f17b7038226fb79ea13a3c028f
Ou Liu <oliu@finivation.com>
2015-03-13 12:07:53 -0400

    closes #1, refs #2

---
+++ Sources/OY.Theory.Shared/IEdge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OY.Theory.Shared
+{
+    public interface IEdge<TVertex>
+    {
+        TVertex Source { get; }
+        TVertex Destination { get; }
+    }
+}
---
+++ Sources/OY.Theory.Shared/IGraph.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OY.Theory.Shared
+{
+    public interface IGraph<TVertex, TEdge> where TEdge : IEdge<TVertex>
+    {
+        bool IsDirected { get; }
+        bool IsMulti { get; }
+    }
+}
