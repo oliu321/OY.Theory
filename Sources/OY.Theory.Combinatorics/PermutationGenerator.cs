cf8858077defaf91c83f7586caf3afca1bf0e12c
Ou Liu <oliu@finivation.com>
2015-03-14 12:46:47 -0400

    refs #2

---
+++ Sources/OY.Theory.Combinatorics/PermutationGenerator.cs
@@ -0,0 +1,27 @@
+﻿/// <copyright file="PermutationGenerator.cs" company="Integration Co, Inc.">
+///     Copyrights: ©2015   Integration Co, Inc. (DBA Finivation Software)
+/// </copyright>
+/// <author>Ou</author>
+
+using OY.Theory.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OY.Theory.Combinatorics
+{
+    public static class PermutationGenerator
+    {
+        public static int[] GenerateRandomPermutation(int length)
+        {
+            return null;
+        }
+
+        public static int[] GenerateRandomPermutation(int length, IRandomGenerator randomGenerator)
+        {
+            return null;
+        }
+    }
+}
