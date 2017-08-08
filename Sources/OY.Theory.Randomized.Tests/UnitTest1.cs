d73376c47c59838eb5f277c8ee3107954db730c5
Ou Liu <oliu@finivation.com>
2017-08-08 17:37:59 -0400

    Add a crypto safe random generator (within .Net)

---
+++ Sources/OY.Theory.Randomized.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OY.Theory.Randomized.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Sources/OY.Theory.Randomized/Distribution/CryptoSafeRandomGenerator.cs
@@ -0,0 +1,35 @@
+﻿using OY.Theory.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OY.Theory.Randomized.Distribution
+{
+    public class CryptoSafeRandomGenerator : IRandomGenerator
+    {
+        private RNGCryptoServiceProvider rng;
+        private byte[] buffer;
+        private DefaultRandomGenerator randomHelper;
+
+        public CryptoSafeRandomGenerator()
+        {
+            this.rng = new RNGCryptoServiceProvider();
+            this.buffer = new byte[4];
+            this.randomHelper = new DefaultRandomGenerator();
+        }
+        public int Next()
+        {
+            this.rng.GetBytes(this.buffer);
+            return BitConverter.ToInt32(this.buffer, 0);
+        }
+        
+        public int Next(int maxValue)
+        {
+            // The below is a trick to make sure the random value are picked uniformly from range [0, maxValue)
+            return (int)(((long)this.randomHelper.Next(maxValue) + (long)this.Next()) % maxValue);
+        }
+    }
+}
